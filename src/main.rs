0b78872ac85b6f57d7791b56fc26e426461a6204
rootabeta <rootabeta@protonmail.com>
2025-01-05 16:35:13 -0500

    Added headless options

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,43 @@
+use clap::Parser;
 use std::io;
 use std::io::Write;
 use yeetseeker::SheetBuilder;
 
+#[derive(Parser)]
+struct Args { 
+    /// User's main nation
+    main_nation: Option<String>,
+    /// Region to compile sheet for
+    region: Option<String>,
+    /// Clobber nations.xml.gz
+    #[arg(short, long, default_value_t = false)]
+    force_update: bool
+}
+
 fn main() {
+    let args = Args::parse();
+
     let mut main_nation = String::new();
     let mut region = String::new();
+    if args.main_nation.is_some() {
+        main_nation = args.main_nation.unwrap()
+    } else { 
+        print!("Main nation: ");
+        io::stdout().flush().unwrap();
+        let _ = io::stdin().read_line(&mut main_nation).unwrap();
+    }
 
-    print!("Main nation: ");
-    io::stdout().flush().unwrap();
-    let _ = io::stdin().read_line(&mut main_nation).unwrap();
-
-    print!("Region: ");
-    io::stdout().flush().unwrap();
-    let _ = io::stdin().read_line(&mut region).unwrap();
+    if args.region.is_some() { 
+        region = args.region.unwrap();
+    } else { 
+        print!("Region: ");
+        io::stdout().flush().unwrap();
+        let _ = io::stdin().read_line(&mut region).unwrap();
+    }
 
     if let Ok(builder) = SheetBuilder::new(&main_nation) { 
         // Ask user to overwrite file, or default true if not present
-        let update_archive = builder.check_archive();
+        let update_archive = args.force_update || builder.check_archive();
 
         // If we need to update the archive, do so
         if update_archive { 
